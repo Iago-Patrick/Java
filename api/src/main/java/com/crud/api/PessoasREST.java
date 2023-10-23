18a5c766f7c527201755723d0e9e52702d73ac48
Iago Patrick <100448015+Iago-Patrick@users.noreply.github.com>
2023-10-23 20:05:43 -0300

    Update PessoasREST.java

--- api/src/main/java/com/crud/api/PessoasREST.java
+++ api/src/main/java/com/crud/api/PessoasREST.java
@@ -1,28 +1,42 @@
 package com.crud.api;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.crud.api.database.Repositoriopessoa;
 import com.crud.api.entidade.Pessoas;
 import java.util.*;
 
+@RestController
+@RequestMapping("/pessoa")
+
 public class PessoasREST {
     @Autowired
     private Repositoriopessoa repositorio;
-
+    
+    @GetMapping
     public List<Pessoas>listar()
     {
         return repositorio.findAll();
     }
-
-    public void salva (Pessoas pessoas)
+    @PostMapping
+    public void salva (@RequestBody Pessoas pessoas)
     {
         repositorio.save(pessoas);
     }
-    public void alterar (Pessoas pessoas)
+    @PutMapping
+    public void alterar (@RequestBody Pessoas pessoas)
     {
         if(pessoas.getId() >0)
             repositorio.save(pessoas);
     }
-    public void excluir(Pessoas pessoas)
+    @DeleteMapping
+    public void excluir(@RequestBody Pessoas pessoas)
     {
         repositorio.delete(pessoas);
     }  
